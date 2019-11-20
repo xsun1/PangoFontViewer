3d048b1dc8d8e4c65268ab3384d2a653b6cf2294
Bill Sun <bill.sun@scala.com>
2019-11-20 11:03:06 -0500

    updated

--- main.c
+++ main.c
@@ -15,10 +15,153 @@
 
 gchar buf[256];
 PangoFontDescription  *g_font_desc = NULL;
-gchar* plaintext  = "AVWA This is a list of answers to questions that are frequently asked by new users to cairo.";
+gchar* plaintext  = "AVWA This is a list of answers to questions that are frequently asked by new users to cairo.😀 ⻤ 🥰";
 float bgcolor[] = {0.0, 0.0, 0.0};
 float fgcolor[] = {1.0, 1.0, 1.0};
 
+
+////////////////////////// Read and Write a bmp file on disk ////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define DATA_OFFSET_OFFSET 0x000A
+#define WIDTH_OFFSET 0x0012
+#define HEIGHT_OFFSET 0x0016
+#define BITS_PER_PIXEL_OFFSET 0x001C
+#define HEADER_SIZE 14
+#define INFO_HEADER_SIZE 40
+#define NO_COMPRESION 0
+#define MAX_NUMBER_OF_COLORS 0
+#define ALL_COLORS_REQUIRED 0
+
+typedef unsigned int int32;
+typedef short int16;
+typedef unsigned char byte;
+
+//***Inputs*****
+//fileName: The name of the file to open 
+//***Outputs****
+//pixels: A pointer to a byte array. This will contain the pixel data
+//width: An int pointer to store the width of the image in pixels
+//height: An int pointer to store the height of the image in pixels
+//bytesPerPixel: An int pointer to store the number of bytes per pixel that are used in the image
+void ReadImage(const char *fileName,byte **pixels, int32 *width, int32 *height, int32 *bytesPerPixel)
+{
+        //Open the file for reading in binary mode
+        FILE *imageFile = fopen(fileName, "rb");
+        //Read data offset
+        int32 dataOffset;
+        fseek(imageFile, DATA_OFFSET_OFFSET, SEEK_SET);
+        fread(&dataOffset, 4, 1, imageFile);
+        //Read width
+        fseek(imageFile, WIDTH_OFFSET, SEEK_SET);
+        fread(width, 4, 1, imageFile);
+        //Read height
+        fseek(imageFile, HEIGHT_OFFSET, SEEK_SET);
+        fread(height, 4, 1, imageFile);
+        //Read bits per pixel
+        int16 bitsPerPixel;
+        fseek(imageFile, BITS_PER_PIXEL_OFFSET, SEEK_SET);
+        fread(&bitsPerPixel, 2, 1, imageFile);
+        //Allocate a pixel array
+        *bytesPerPixel = ((int32)bitsPerPixel) / 8;
+
+        //Rows are stored bottom-up
+        //Each row is padded to be a multiple of 4 bytes. 
+        //We calculate the padded row size in bytes
+        int paddedRowSize = (int)(4 * ceil((float)(*width) / 4.0f))*(*bytesPerPixel);
+        //We are not interested in the padded bytes, so we allocate memory just for
+        //the pixel data
+        int unpaddedRowSize = (*width)*(*bytesPerPixel);
+        //Total size of the pixel data in bytes
+        int totalSize = unpaddedRowSize*(*height);
+        *pixels = (byte*)malloc(totalSize);
+        //Read the pixel data Row by Row.
+        //Data is padded and stored bottom-up
+        int i = 0;
+        //point to the last row of our pixel array (unpadded)
+        byte *currentRowPointer = *pixels+((*height-1)*unpaddedRowSize);
+        for (i = 0; i < *height; i++)
+        {
+                //put file cursor in the next row from top to bottom
+	        fseek(imageFile, dataOffset+(i*paddedRowSize), SEEK_SET);
+	        //read only unpaddedRowSize bytes (we can ignore the padding bytes)
+	        fread(currentRowPointer, 1, unpaddedRowSize, imageFile);
+	        //point to the next row (from bottom to top)
+	        currentRowPointer -= unpaddedRowSize;
+        }
+
+        fclose(imageFile);
+}
+
+//***Inputs*****
+//fileName: The name of the file to save 
+//pixels: Pointer to the pixel data array
+//width: The width of the image in pixels
+//height: The height of the image in pixels
+//bytesPerPixel: The number of bytes per pixel that are used in the image
+void WriteImage(const char *fileName, byte *pixels, int32 width, int32 height,int32 bytesPerPixel)
+{
+        //Open file in binary mode
+        FILE *outputFile = fopen(fileName, "wb");
+        //*****HEADER************//
+        //write signature
+        const char *BM = "BM";
+        fwrite(&BM[0], 1, 1, outputFile);
+        fwrite(&BM[1], 1, 1, outputFile);
+        //Write file size considering padded bytes
+        int paddedRowSize = (int)(4 * ceil((float)width/4.0f))*bytesPerPixel;
+        int32 fileSize = paddedRowSize*height + HEADER_SIZE + INFO_HEADER_SIZE;
+        fwrite(&fileSize, 4, 1, outputFile);
+        //Write reserved
+        int32 reserved = 0x0000;
+        fwrite(&reserved, 4, 1, outputFile);
+        //Write data offset
+        int32 dataOffset = HEADER_SIZE+INFO_HEADER_SIZE;
+        fwrite(&dataOffset, 4, 1, outputFile);
+
+        //*******INFO*HEADER******//
+        //Write size
+        int32 infoHeaderSize = INFO_HEADER_SIZE;
+        fwrite(&infoHeaderSize, 4, 1, outputFile);
+        //Write width and height
+        fwrite(&width, 4, 1, outputFile);
+        fwrite(&height, 4, 1, outputFile);
+        //Write planes
+        int16 planes = 1; //always 1
+        fwrite(&planes, 2, 1, outputFile);
+        //write bits per pixel
+        int16 bitsPerPixel = bytesPerPixel * 8;
+        fwrite(&bitsPerPixel, 2, 1, outputFile);
+        //write compression
+        int32 compression = NO_COMPRESION;
+        fwrite(&compression, 4, 1, outputFile);
+        //write image size (in bytes)
+        int32 imageSize = width*height*bytesPerPixel;
+        fwrite(&imageSize, 4, 1, outputFile);
+        //write resolution (in pixels per meter)
+        int32 resolutionX = 11811; //300 dpi
+        int32 resolutionY = 11811; //300 dpi
+        fwrite(&resolutionX, 4, 1, outputFile);
+        fwrite(&resolutionY, 4, 1, outputFile);
+        //write colors used 
+        int32 colorsUsed = MAX_NUMBER_OF_COLORS;
+        fwrite(&colorsUsed, 4, 1, outputFile);
+        //Write important colors
+        int32 importantColors = ALL_COLORS_REQUIRED;
+        fwrite(&importantColors, 4, 1, outputFile);
+        //write data
+        int i = 0;
+        int unpaddedRowSize = width*bytesPerPixel;
+        for ( i = 0; i < height; i++)
+        {
+                //start writing from the beginning of last row in the pixel array
+                int pixelOffset = ((height - i) - 1)*unpaddedRowSize;
+                fwrite(&pixels[pixelOffset], 1, paddedRowSize, outputFile);	
+        }
+        fclose(outputFile);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 void destroy( GtkWidget *widget,gpointer   data )
 {
@@ -90,23 +233,37 @@ gboolean on_expose_event2(GtkWidget *widget,
 	/* ------------------------------------------------------------ */
 	/*                   I N I T I A L I Z E                        */
 	/* ------------------------------------------------------------ */
-
+//#define USE_COLOR_BMP
 	/* FT buffer */
 	FT_Bitmap_New(&bmp);
 	bmp.rows = height;
 	bmp.width = width;
+#ifdef USE_COLOR_BMP
+	bmp.buffer = (unsigned char*)calloc(bmp.rows * bmp.width, 4);
+#else
 	bmp.buffer = (unsigned char*)calloc(bmp.rows * bmp.width, 1);
+#endif	
 	if (NULL == bmp.buffer) {
 		printf("+ error: cannot allocate the buffer for the output bitmap.\n");
 		exit(EXIT_FAILURE);
 	}
  
 	/* create our "canvas" */
+#ifdef USE_COLOR_BMP	
+	bmp.pitch = ((width + 3) & -4) * 4; 
+	bmp.pixel_mode = FT_PIXEL_MODE_BGRA; /*< Grayscale*/
+#else
 	bmp.pitch = (width + 3) & -4;
 	bmp.pixel_mode = FT_PIXEL_MODE_GRAY; /*< Grayscale*/
+#endif	
 	bmp.num_grays = 256;
+#ifdef USE_COLOR_BMP	
+	stride = cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, width);
+	surf = cairo_image_surface_create_for_data(bmp.buffer, CAIRO_FORMAT_ARGB32, width, height, stride);
+#else
 	stride = cairo_format_stride_for_width(CAIRO_FORMAT_A8, width);
 	surf = cairo_image_surface_create_for_data(bmp.buffer, CAIRO_FORMAT_A8, width, height, stride);
+#endif
  
 	if (CAIRO_STATUS_SUCCESS != cairo_surface_status(surf)) {
 		printf("+ error: couldn't create the surface.\n");
@@ -162,7 +319,7 @@ gboolean on_expose_event2(GtkWidget *widget,
     "</span>"
     "<tt> hello </tt>"
     "<span font_family=\"sans\" font_stretch=\"ultracondensed\" letter_spacing=\"500\" font_weight=\"light\"> SANS</span>"
-    "<span foreground=\"#FFCC00\"> colored</span>"
+    "<span foreground=\"#FFCC00\"> colored  😀 ⺁ ⻤ 🥰</span>"
     "";
  
 //  gchar* plaintext ;
@@ -191,6 +348,27 @@ gboolean on_expose_event2(GtkWidget *widget,
 	/* ------------------------------------------------------------ */
 	/*               O U T P U T  A N D  C L E A N U P              */
 	/* ------------------------------------------------------------ */
+
+	//Write to bmp
+	/* 
+	for(int j = 0; j < height; j++)
+	{
+		for(int i = 0; i < width; i++)
+		{
+			unsigned char temp[4];
+			p = bmp.buffer + 4 * (i + j * width);
+			temp[0] = p[3];
+			temp[1] = p[2];
+			temp[2] = p[1];
+			temp[3] = p[0];
+			p[0] = temp[0];
+			p[1] = temp[1];
+			p[2] = temp[2];
+			p[3] = temp[3];
+		}
+	}
+	 WriteImage("img2.bmp", bmp.buffer, width, height, 4);
+	*/
 
 	/* write to png */
 	status = cairo_surface_write_to_png(surf, "test_font.png");
@@ -299,7 +477,7 @@ gboolean on_expose_event4(GtkWidget *widget,
 		cairo_paint (cr);
 		cairo_set_source_rgb (cr, fgcolor[0], fgcolor[1], fgcolor[2]);
 		layout = pango_cairo_create_layout (cr);  // This function is the most convenient way to use Cairo with Pango, however 
-																	//  it is slightly inefficient since it creates a separate PangoContext object for each layout.
+								//  it is slightly inefficient since it creates a separate PangoContext object for each layout.
 		// Make sure the DPI (resolution) same on both Cairo context and Pango context
 		PangoContext *pCtx = pango_layout_get_context(layout);
 		pango_cairo_context_set_resolution(pCtx, 72);
